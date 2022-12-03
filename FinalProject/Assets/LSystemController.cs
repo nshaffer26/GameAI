fadbb1143dafd0eca38eea6ef46c19f1c8b267f3
nshaffer26 <nshaffer26@outlook.com>
2022-12-03 06:41:50 -0500

    Fix overlap handling
    The overlap handling now works correctly, i.e., a path will not be made if it causes an overlap with an existing path.
    
    TODO:
    There is an issue where if a branch ends in the middle of a four-way intersection, the ending path cannot be made.

--- FinalProject/Assets/LSystemController.cs
+++ FinalProject/Assets/LSystemController.cs
@@ -3,11 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine.Rendering;
+using Unity.VisualScripting;
 
 public class LSystemController : MonoBehaviour
 {
-
-
     // for defining the language and rules
     Hashtable ruleHash = new Hashtable(100);
 
@@ -17,7 +16,7 @@ public class LSystemController : MonoBehaviour
     List<byte> lang;
     GameObject contents;
     float angleToUse = 90f;
-    public int iterations = 4;
+    int iterations = 4;
 
     // current location and angle
     Vector3 position = new Vector3(0, 0, 0);
@@ -47,6 +46,9 @@ public class LSystemController : MonoBehaviour
 
     void Start()
     {
+        // DEBUG
+        //Random.InitState(42);
+        //
 
         // for timing start/finish of the rule generation and display
         // can be commented out
@@ -65,72 +67,14 @@ public class LSystemController : MonoBehaviour
         // we set the start with the expected max size of the language iteration
         start = new List<byte>(100);
 
-        //variables : 0, 1
-        //constants: [, ]
-        //axiom  : 0
-        //rules  : (1 → 11), (0 → 1[0]0)
-        // Second example LSystem from 
-        // http://en.wikipedia.org/wiki/L-system
-        // uncomment the following for the lsystem above:
-        //start.Add(0);
-        //lang = start;
-        //// 1 -> 11
-        //byte[] firstRule = new byte[] { 1, 1 };
-        //ruleHash.Add((byte)1, firstRule);
-        //// 0 -> 1[0]0
-        //// note: we/re using bytes rather than alpha text here, so we just need [ and ] to be
-        //// an easy to remember number 6 and 9 look like opposites to me and I chose them.
-        //byte[] secondRule = new byte[] { 1, 6, 0, 9, 0 };
-        //ruleHash.Add((byte)0, secondRule);
-        //angleToUse = 45f;
-        //run(iterations);
-        //// now print out the time for gen
-        //watch.Stop();
-        //UnityEngine.Debug.Log("Time for generation took: " + watch.ElapsedMilliseconds);
-        //watch.Reset();
-        //watch.Start();
-        //// print out the display time
-        //vertices = new List<vertexInfo>(lang.Count);
-        //indices = new List<int>(lang.Count);
-        //display2();
-
-
-
-        // Weed type plant example from: 
-        // http://en.wikipedia.org/wiki/L-system
-        // rules: X = 0, F = 1, 
-        // [ = 4, ] = 5
-        // + = 2, - = 3
-        // Uncomment the following for a weed type lsystem:
-
-        //start.Add(0);
-        ////X -> F-[[X]+X]+F[+FX]-X
-        //// keep in mind that all the letters are translated into numbers for the byte array
-        //byte[] firstRule = new byte[] { 1, 3, 4, 4, 0, 5, 2, 0, 5, 2, 1, 4, 2, 1, 0, 5, 3, 0 };
-        //ruleHash.Add((byte)0, firstRule);
-        ////F -> FF 
-        //byte[] secondRule = new byte[] { 1, 1 };
-        //ruleHash.Add((byte)1, secondRule);
-
-        //angleToUse = 25f;
-        //run(iterations);
-        //// now print out the time for gen
-        //watch.Stop();
-        //UnityEngine.Debug.Log("Time for generation took: " + watch.ElapsedMilliseconds);
-        //UnityEngine.Debug.Log("Size of lang is: " + lang.Count);
-        //watch.Reset();
-        //// print out the time for display
-        //watch.Start();
-        //vertices = new List<vertexInfo>(lang.Count);
-        //indices = new List<int>(lang.Count);
-        //display3();
-
-        //start.Add(8);
+        // Axiom  : 809
+        start.Add(8);
         start.Add(0);
+        start.Add(9);
         byte[] rule;
 
         // All characters are translated into numbers for the byte array:
-        // X=0 , F=1 , +=2 , -=3 , [=4 , ]=5
+        // X=0 , F=1 , +=2 , -=3 , [=4 , ]=5, S=8, E=9
 
         List<byte[]> rules0 = new List<byte[]>();
         // X -> F
@@ -155,49 +99,65 @@ public class LSystemController : MonoBehaviour
         // F -> FX
         rule = new byte[] { 1, 0 };
         rules1.Add(rule);
-        // F -> [F]F
-        rule = new byte[] { 4, 1, 5, 1 };
+        // F -> [+F]X
+        rule = new byte[] { 4, 2, 1, 5, 0 };
         rules1.Add(rule);
-        // F -> [F]X
-        rule = new byte[] { 4, 1, 5, 0 };
+        // F -> [-F]X
+        rule = new byte[] { 4, 3, 1, 5, 0 };
         rules1.Add(rule);
-        // F -> [X]X
-        rule = new byte[] { 4, 0, 5, 0 };
+        // F -> [+F]F
+        rule = new byte[] { 4, 2, 1, 5, 1 };
         rules1.Add(rule);
-        // F -> [X]F
-        rule = new byte[] { 4, 0, 5, 1 };
+        // F -> [-F]F
+        rule = new byte[] { 4, 3, 1, 5, 1 };
         rules1.Add(rule);
-        //// F -> [+F]X
-        //rule = new byte[] { 4, 2, 1, 5, 0 };
-        //rules1.Add(rule);
-        //// F -> [+X]F
-        //rule = new byte[] { 4, 2, 0, 5, 1 };
-        //rules1.Add(rule);
-        //// F -> [-F]X
-        //rule = new byte[] { 4, 3, 1, 5, 0 };
-        //rules1.Add(rule);
-        //// F -> [-X]F
-        //rule = new byte[] { 4, 3, 0, 5, 1 };
-        //rules1.Add(rule);
 
         ruleHash.Add((byte)1, rules1);
+
+        List<byte[]> rules8 = new List<byte[]>();
+        // S -> +S
+        rule = new byte[] { 2, 8 };
+        rules8.Add(rule);
+        // S -> ++S
+        rule = new byte[] { 2, 2, 8 };
+        rules8.Add(rule);
+        // S -> +++S
+        rule = new byte[] { 2, 2, 2, 8 };
+        rules8.Add(rule);
+        // S -> S
+        rule = new byte[] { 8 };
+        rules8.Add(rule);
+
+        ruleHash.Add((byte)8, rules8);
+
+        List<byte[]> rules9 = new List<byte[]>();
+        // E -> +E
+        rule = new byte[] { 2, 9 };
+        rules9.Add(rule);
+        // E -> ++E
+        rule = new byte[] { 2, 2, 9 };
+        rules9.Add(rule);
+        // E -> +++E
+        rule = new byte[] { 2, 2, 2, 9 };
+        rules9.Add(rule);
+        // E -> E
+        rule = new byte[] { 9 };
+        rules9.Add(rule);
+
+        ruleHash.Add((byte)9, rules9);
 
         vertices = new List<vertexInfo>();
         indices = new List<int>();
         run(iterations);
 
-        string s = "";
-        foreach (byte b in lang)
-        {
-            s += b;
-        }
-        print(s);
-
         // now print out the time for gen
         watch.Stop();
 
-        UnityEngine.Debug.Log("Time for generation took: " + watch.ElapsedMilliseconds);
-        UnityEngine.Debug.Log("Size of lang is: " + lang.Count);
+        // DEBUG
+        //UnityEngine.Debug.Log("Time for generation took: " + watch.ElapsedMilliseconds);
+        //UnityEngine.Debug.Log("Size of lang is: " + lang.Count);
+        //
+
         watch.Reset();
 
         // print out the time for display
@@ -207,68 +167,58 @@ public class LSystemController : MonoBehaviour
 
         watch.Stop();
 
-        UnityEngine.Debug.Log("Time for display took: " + watch.ElapsedMilliseconds);
-        UnityEngine.Debug.Log("Count of vertices in list: " + vertices.Count);
+        // DEBUG
+        //UnityEngine.Debug.Log("Time for display took: " + watch.ElapsedMilliseconds);
+        //UnityEngine.Debug.Log("Count of vertices in list: " + vertices.Count);
+        //
     }
 
     // Get a rule from a given letter that's in our array
     byte[] getRule(byte[] input)
     {
-        byte[] chosenRule = input;
+        byte[] chosenRule = new byte[input.Length];
+        input.CopyTo(chosenRule, 0);
 
         if (ruleHash.ContainsKey(input[0]))
         {
             List<byte[]> rules = ruleHash[input[0]] as List<byte[]>;
-            chosenRule = rules[UnityEngine.Random.Range(0, rules.Count)];
-            List<byte> potentialRule = new List<byte>(chosenRule);
 
-            List<vertexInfo> verticesTemp = vertices;
-            List<int> indicesTemp = indices;
-            Vector3 positionTemp = position;
-            float angleTemp = angle;
-            Stack<float> positionsTemp = positions;
-            Stack<float> anglesTemp = angles;
-
-            int stop = 0;
-            while (!checkVertices(getVerticies(potentialRule)) && stop < 10)
+            // Randomize rules
+            Stack<int> ruleIndices = new Stack<int>();
+            while (ruleIndices.Count < rules.Count)
             {
-                chosenRule = rules[UnityEngine.Random.Range(0, rules.Count)];
-                potentialRule = new List<byte>(chosenRule);
+                int i = UnityEngine.Random.Range(0, rules.Count);
+                if (ruleIndices.Contains(i))
+                {
+                    continue;
+                }
 
-                // Reset positions and angles
-                vertices = verticesTemp;
-                indices = indicesTemp;
-                position = positionTemp;
-                angle = angleTemp;
-                positions = positionsTemp;
-                angles = anglesTemp;
-
-                stop++;
+                ruleIndices.Push(i);
             }
 
-            List<vertexInfo> potentialVertices = getVerticies(potentialRule);
-            if (!checkVertices(potentialVertices))
+            // Keep trying rules until there are none left or a valid one is found
+            bool verticesAdded;
+            do
             {
-                chosenRule = input;
-
-                // Reset positions and angles
-                vertices = verticesTemp;
-                indices = indicesTemp;
-                position = positionTemp;
-                angle = angleTemp;
-                positions = positionsTemp;
-                angles = anglesTemp;
-
-                potentialVertices = getVerticies(new List<byte>(chosenRule));
+                chosenRule = rules[ruleIndices.Pop()];
+                List<byte> potentialRule = new List<byte>(chosenRule);
+                verticesAdded = addVerticies(potentialRule);
             }
-            //if (potentialVertices.Count == 2)
-            //{
-            //    int numberOfPoints = vertices.Count;
-            //    int[] indicesForLines = new int[] { 0 + numberOfPoints, 1 + numberOfPoints };
+            while (ruleIndices.Count > 0 && !verticesAdded);
 
-            //    vertices.AddRange(potentialVertices);
-            //    indices.AddRange(indicesForLines);
-            //}
+
+            if (!verticesAdded)
+            {
+                // A rule could not be found, replace with a terminating character
+                chosenRule = new byte[1] { 0 };
+                addVerticies(new List<byte>(chosenRule));
+                // TODO: This causes an occasional issue where the end cannot spawn, i.e., at the end of a four way branch
+            }
+        }
+        else
+        {
+            // There isn't a rule for this byte, see if vertices can be added
+            addVerticies(new List<byte>(chosenRule));
         }
 
         return chosenRule;
@@ -287,6 +237,14 @@ public class LSystemController : MonoBehaviour
 
         for (int i = 0; i < iterations; i++)
         {
+            // Reset positions and angles
+            vertices.Clear();
+            indices.Clear();
+            position = Vector3.zero;
+            angle = 0f;
+            positions.Clear();
+            angles.Clear();
+
             currentCount = currentList.Count;
             for (int j = 0; j < currentCount; j++)
             {
@@ -301,15 +259,35 @@ public class LSystemController : MonoBehaviour
         }
 
         lang = currentList;
-        //lang.Add(9);
+
+        // DEBUG
+        string s = "Vertices (" + vertices.Count + "): ";
+        foreach (vertexInfo v in vertices)
+        {
+            s += v.pos + ", ";
+        }
+        print(s);
+
+        s = "Lang (" + lang.Count + "): ";
+        foreach (byte b in lang)
+        {
+            s += b;
+        }
+        print(s);
+        //
     }
 
-    List<vertexInfo> getVerticies(List<byte> str)
+    bool addVerticies(List<byte> str)
     {
         List<vertexInfo> potentialVertices = new List<vertexInfo>();
 
-        float posx = position.x;
-        float posy = position.y;
+        Vector3 positionTemp = position;
+        float angleTemp = angle;
+        Stack<float> positionsTemp = new Stack<float>(positions);
+        Stack<float> anglesTemp = new Stack<float>(angles);
+
+        float posx = positionTemp.x;
+        float posy = positionTemp.y;
 
         // location and rotation to draw towards
         Vector3 newPosition;
@@ -335,58 +313,85 @@ public class LSystemController : MonoBehaviour
 
                     // draw a line 
                     posy += initial_length;
-                    rotated = rotate(position, new Vector3(position.x, posy, 0), angle);
+                    rotated = rotate(positionTemp, new Vector3(positionTemp.x, posy, 0), angleTemp);
                     newPosition = new Vector3(rotated.x, rotated.y, 0);
 
-                    potentialVertices.AddRange(addLineToMesh(lineMesh, position, newPosition, color));
+                    potentialVertices.AddRange(addLineToMesh(lineMesh, positionTemp, newPosition, color));
 
                     // set up for the next draw
-                    position = newPosition;
+                    positionTemp = newPosition;
                     posx = newPosition.x;
                     posy = newPosition.y;
 
                     break;
                 case 2:
                     // Turn left 90
-                    angle += angleToUse;
+                    angleTemp += angleToUse;
 
                     break;
                 case 3:
                     // Turn right 90
-                    angle -= angleToUse;
+                    angleTemp -= angleToUse;
 
                     break;
                 case 4:
                     //[: push position and angle
-                    positions.Push(posy);
-                    positions.Push(posx);
-                    float currentAngle = angle;
-                    angles.Push(currentAngle);
+                    positionsTemp.Push(posy);
+                    positionsTemp.Push(posx);
+                    float currentAngle = angleTemp;
+                    anglesTemp.Push(currentAngle);
 
                     break;
                 case 5:
                     //]: pop position and angle
-                    posx = positions.Pop();
-                    posy = positions.Pop();
-                    position = new Vector3(posx, posy, 0);
-                    angle = angles.Pop();
+                    posx = positionsTemp.Pop();
+                    posy = positionsTemp.Pop();
+                    positionTemp = new Vector3(posx, posy, 0);
+                    angleTemp = anglesTemp.Pop();
 
                     break;
                 default: break;
             }
         }
 
-        return potentialVertices;
+        if (checkVertices(potentialVertices))
+        {
+            // Apply everything
+            position = positionTemp;
+            angle = angleTemp;
+            positions = new Stack<float>(positionsTemp);
+            angles = new Stack<float>(anglesTemp);
+
+            for (int i = 0; i < potentialVertices.Count; i += 2)
+            {
+                // The vertices are valid, add them
+                int numberOfPoints = vertices.Count;
+                int[] indicesForLines = new int[] { 0 + numberOfPoints, 1 + numberOfPoints };
+
+                vertices.Add(potentialVertices[i]);
+                vertices.Add(potentialVertices[i + 1]);
+                indices.AddRange(indicesForLines);
+            }
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     bool checkVertices(List<vertexInfo> verts)
     {
         // Don't check the first vertex since it will always be the last in vertices
-        for (int i = 1; i < verts.Count - 1; i++)
+        for (int i = 1; i < verts.Count; i++)
         {
-            if (vertices.Contains(verts[i]))
+            for (int j = 0; j < vertices.Count; j++)
             {
-                return false;
+                if (vertices[j].pos == verts[i].pos)
+                {
+                    return false;
+                }
             }
         }
 
@@ -395,8 +400,8 @@ public class LSystemController : MonoBehaviour
 
     void display4()
     {
-        vertices.TrimExcess();
-        indices.TrimExcess();
+        //vertices.TrimExcess();
+        //indices.TrimExcess();
 
         // after we recreate the mesh we need to assign it to the original object
         MeshUpdateFlags flags = MeshUpdateFlags.DontNotifyMeshUsers & MeshUpdateFlags.DontRecalculateBounds
@@ -430,11 +435,11 @@ public class LSystemController : MonoBehaviour
     vertexInfo[] addLineToMesh(Mesh mesh, Vector3 from, Vector3 to, Color color)
     {
         vertexInfo[] lineVerts = new vertexInfo[] { new vertexInfo(from, color), new vertexInfo(to, color) };
-        int numberOfPoints = vertices.Count;
-        int[] indicesForLines = new int[] { 0 + numberOfPoints, 1 + numberOfPoints };
+        //int numberOfPoints = vertices.Count;
+        //int[] indicesForLines = new int[] { 0 + numberOfPoints, 1 + numberOfPoints };
 
-        vertices.AddRange(lineVerts);
-        indices.AddRange(indicesForLines);
+        //vertices.AddRange(lineVerts);
+        //indices.AddRange(indicesForLines);
 
         return lineVerts;
     }
@@ -473,5 +478,15 @@ public class LSystemController : MonoBehaviour
             indices.Add(numberOfPoints + i);
             angle += (360f / segments);
         }
+    }
+
+    void ResetBranch(Vector3 positionTemp, float angleTemp, Stack<float> positionsTemp, Stack<float> anglesTemp)
+    {
+        //vertices = new List<vertexInfo>(verticesTemp);
+        //indices = new List<int>(indicesTemp);
+        position = positionTemp;
+        angle = angleTemp;
+        positions = new Stack<float>(positionsTemp);
+        angles = new Stack<float>(anglesTemp);
     }
 }
