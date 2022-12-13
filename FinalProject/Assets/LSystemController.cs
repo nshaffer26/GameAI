3367a37435d9c95f1496e1a4042627410609f17e
nshaffer26 <nshaffer26@outlook.com>
2022-12-13 08:04:12 -0500

    Add up/down paths to dungeon, refine L-System rules

--- FinalProject/Assets/LSystemController.cs
+++ FinalProject/Assets/LSystemController.cs
@@ -7,7 +7,7 @@ using Unity.VisualScripting;
 
 public class LSystemController : MonoBehaviour
 {
-    // for defining the language and rules
+    // For defining the language and rules
     Hashtable ruleHash = new Hashtable(100);
 
     float initial_length = 6.0f;
@@ -16,7 +16,9 @@ public class LSystemController : MonoBehaviour
     List<byte> lang;
     GameObject contents;
     float angleToUse = 90f;
-    int iterations = 10;
+    int iterations = 6;
+
+    public bool multiLevel = true;
 
     // current location and angle
     Vector3 position = new Vector3(0, 0, 0);
@@ -54,6 +56,7 @@ public class LSystemController : MonoBehaviour
     {
         // DEBUG
         //Random.InitState(42);
+        //Random.InitState(2222);
         //
 
         roomManager = GameObject.Find("RoomManager").GetComponent<RoomManager>();
@@ -82,7 +85,7 @@ public class LSystemController : MonoBehaviour
         byte[] rule;
 
         // All characters are translated into numbers for the byte array:
-        // X=0 , F=1 , +=2 , -=3 , [=4 , ]=5, S=8, E=9
+        // X=0 , F=1 , <=2 , >=3 , [=4 , ]=5, U=6, D=7, S=8, E=9, *=10
 
         List<byte[]> rules0 = new List<byte[]>();
         // X -> F
@@ -91,48 +94,51 @@ public class LSystemController : MonoBehaviour
         // X -> FF
         rule = new byte[] { 1, 1 };
         rules0.Add(rule);
-        // X -> F-F
-        rule = new byte[] { 1, 3, 1 };
+        // X -> F[>F]F
+        rule = new byte[] { 1, 4, 2, 1, 5, 1 };
         rules0.Add(rule);
-        // X -> F+F
-        rule = new byte[] { 1, 2, 1 };
+        // X -> F[<F]F
+        rule = new byte[] { 1, 4, 3, 1, 5, 1 };
         rules0.Add(rule);
+        if (multiLevel)
+        {
+            // X -> *U*F
+            rule = new byte[] { 10, 6, 10, 1 };
+            rules0.Add(rule);
+            // X -> *D*F
+            rule = new byte[] { 10, 7, 10, 1 };
+            rules0.Add(rule);
+        }
 
         ruleHash.Add((byte)0, rules0);
 
         List<byte[]> rules1 = new List<byte[]>();
-        // F -> +F
-        rule = new byte[] { 2, 1 };
-        rules1.Add(rule);
-        // F -> -F
-        rule = new byte[] { 3, 1 };
-        rules1.Add(rule);
         // F -> FX
         rule = new byte[] { 1, 0 };
         rules1.Add(rule);
-        // F -> [+F]X
-        rule = new byte[] { 4, 2, 1, 5, 0 };
+        // F -> >FX
+        rule = new byte[] { 2, 1, 0 };
         rules1.Add(rule);
-        // F -> [-F]X
-        rule = new byte[] { 4, 3, 1, 5, 0 };
+        // F -> <FX
+        rule = new byte[] { 3, 1, 0 };
         rules1.Add(rule);
-        // F -> [+F]F
+        // F -> [>F]F
         rule = new byte[] { 4, 2, 1, 5, 1 };
         rules1.Add(rule);
-        // F -> [-F]F
+        // F -> [<F]F
         rule = new byte[] { 4, 3, 1, 5, 1 };
         rules1.Add(rule);
 
         ruleHash.Add((byte)1, rules1);
 
         List<byte[]> rules8 = new List<byte[]>();
-        // S -> +S
+        // S -> <S
         rule = new byte[] { 2, 8 };
         rules8.Add(rule);
-        // S -> ++S
+        // S -> <<S
         rule = new byte[] { 2, 2, 8 };
         rules8.Add(rule);
-        // S -> +++S
+        // S -> <<<S
         rule = new byte[] { 2, 2, 2, 8 };
         rules8.Add(rule);
         // S -> S
@@ -150,13 +156,13 @@ public class LSystemController : MonoBehaviour
         ruleHash.Add((byte)8, rules8);
 
         List<byte[]> rules9 = new List<byte[]>();
-        // E -> +E
+        // E -> <E
         rule = new byte[] { 2, 9 };
         rules9.Add(rule);
-        // E -> ++E
+        // E -> <<E
         rule = new byte[] { 2, 2, 9 };
         rules9.Add(rule);
-        // E -> +++E
+        // E -> <<<E
         rule = new byte[] { 2, 2, 2, 9 };
         rules9.Add(rule);
         // E -> E
@@ -250,8 +256,26 @@ public class LSystemController : MonoBehaviour
                 if (input[0] == 9)
                 {
                     // This is an exit and it cannot be skipped
-                    // Choose a random vertex that isn't the start and try to add the exit there
-                    position = vertices[UnityEngine.Random.Range(2, vertices.Count - 1)].pos;
+                    // Choose a random vertex that isn't the start or at a slope and try to add the exit there
+                    int index;
+                    do
+                    {
+                        index = UnityEngine.Random.Range(2, vertices.Count - 1);
+                        position = vertices[index].pos;
+
+                        if (vertices[index].pos.y < vertices[index - 1].pos.y
+                            || vertices[index].pos.y > vertices[index - 1].pos.y
+                            || vertices[index].pos.y < vertices[index + 1].pos.y
+                            || vertices[index].pos.y > vertices[index + 1].pos.y)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    while (true);
                     chosenRule = new byte[1] { 9 };
                     getRule(chosenRule);
                 }
@@ -259,6 +283,7 @@ public class LSystemController : MonoBehaviour
                 {
                     // A rule could not be found, replace with a terminating character
                     chosenRule = new byte[1] { 0 };
+                    print("RULE NOT FOUND");
                     addVerticies(new List<byte>(chosenRule));
                 }
             }
@@ -266,9 +291,15 @@ public class LSystemController : MonoBehaviour
         else
         {
             // There isn't a rule for this byte, see if vertices can be added
+            print("NO RULE");
             addVerticies(new List<byte>(chosenRule));
         }
-
+        string s = "";
+        foreach(byte b in chosenRule)
+        {
+            s += b + ".";
+        }
+        print(s);
         return chosenRule;
     }
 
@@ -285,6 +316,7 @@ public class LSystemController : MonoBehaviour
 
         for (int i = 0; i < iterations; i++)
         {
+            print("ITERATION " + i);
             // Reset positions and angles
             vertices.Clear();
             indices.Clear();
@@ -319,7 +351,7 @@ public class LSystemController : MonoBehaviour
         s = "Lang (" + lang.Count + "): ";
         foreach (byte b in lang)
         {
-            s += b;
+            s += b + ".";
         }
         print(s);
         //
@@ -335,6 +367,7 @@ public class LSystemController : MonoBehaviour
         Stack<float> anglesTemp = new Stack<float>(angles);
 
         float posx = positionTemp.x;
+        float posy = positionTemp.y;
         float posz = positionTemp.z;
 
         // location and rotation to draw towards
@@ -343,48 +376,74 @@ public class LSystemController : MonoBehaviour
 
         Color color = Color.green;
 
+        string s = "";
+        foreach (byte b in str)
+        {
+            s += b + ".";
+        }
+        s += ": ";
+
         // start at 0,0,0
         // Apply all the drawing rules to the lsystem string
         for (int i = 0; i < str.Count; i++)
         {
             byte buff = str[i];
+            s += buff + ".";
             switch (buff)
             {
                 case 0:
+                    s += ")";
                     break;
+                case 6:
+                case 7:
                 case 8:
                 case 9:
+                case 10:
                 case 1:
                     if (buff == 8) color = Color.blue;
-                    if (buff == 9) color = Color.red;
-                    if (buff == 1) color = Color.green;
+                    else if (buff == 9) color = Color.red;
+                    else if (buff == 10)
+                    {
+                        s += "$";
+                        color = Color.yellow;
+                    }
+                    else color = Color.green;
+                    if (buff == 6) posy += Mathf.Round(initial_length / 2);
+                    if (buff == 7) posy -= Mathf.Round(initial_length / 2);
+
+                    if (buff == 1)
+                    {
+                        s += "!";
+                    }
 
                     // draw a line 
                     posz += initial_length;
-                    rotated = rotate(positionTemp, new Vector3(positionTemp.x, 0, posz), angleTemp);
-                    newPosition = new Vector3(rotated.x, 0, rotated.y);
+                    rotated = rotate(positionTemp, new Vector3(positionTemp.x, posy, posz), angleTemp);
+                    newPosition = new Vector3(rotated.x, posy, rotated.y);
 
                     potentialVertices.AddRange(addLineToMesh(lineMesh, positionTemp, newPosition, color));
 
                     // set up for the next draw
                     positionTemp = newPosition;
                     posx = newPosition.x;
+                    posy = newPosition.y;
                     posz = newPosition.z;
 
                     break;
                 case 2:
                     // Turn left 90
-                    angleTemp += angleToUse;
+                    angleTemp -= angleToUse;
 
                     break;
                 case 3:
                     // Turn right 90
-                    angleTemp -= angleToUse;
+                    angleTemp += angleToUse;
 
                     break;
                 case 4:
                     //[: push position and angle
                     positionsTemp.Push(posz);
+                    positionsTemp.Push(posy);
                     positionsTemp.Push(posx);
                     float currentAngle = angleTemp;
                     anglesTemp.Push(currentAngle);
@@ -393,17 +452,21 @@ public class LSystemController : MonoBehaviour
                 case 5:
                     //]: pop position and angle
                     posx = positionsTemp.Pop();
+                    posy = positionsTemp.Pop();
                     posz = positionsTemp.Pop();
-                    positionTemp = new Vector3(posx, 0, posz);
+                    positionTemp = new Vector3(posx, posy, posz);
                     angleTemp = anglesTemp.Pop();
 
                     break;
-                default: break;
+                default:
+                    s += "x";
+                    break;
             }
         }
 
         if (checkVertices(potentialVertices))
         {
+            print(s);
             // Apply everything
             position = positionTemp;
             angle = angleTemp;
@@ -436,8 +499,80 @@ public class LSystemController : MonoBehaviour
         {
             for (int j = 0; j < vertices.Count; j++)
             {
+                // Check for colliding endpoints
                 if (vertices[j].pos == verts[i].pos)
                 {
+                    return false;
+                }
+
+                // Check for straight branching paths coming out of the same vertex as an up/down path
+                Vector3 above = new Vector3(verts[i].pos.x, verts[i].pos.y + Mathf.Round(initial_length / 2), verts[i].pos.z);
+                Vector3 below = new Vector3(verts[i].pos.x, verts[i].pos.y - Mathf.Round(initial_length / 2), verts[i].pos.z);
+                if ((vertices[j].pos == above || vertices[j].pos == below)
+                    && j > 0 && vertices[j - 1].pos == verts[i - 1].pos)
+                {
+                    return false;
+                }
+
+                // Check for crossing diagonals
+                if (j > 0 && (vertices[j - 1].pos == above || vertices[j - 1].pos == below))
+                {
+                    return false;
+                }
+
+                // Prevent from moving up/down if there is an existing vertex above/below current
+                // Note: Not technically an overlap, but it makes the gemoetry look strange
+                Vector3 prevAbove = new Vector3(verts[i - 1].pos.x, verts[i - 1].pos.y + Mathf.Round(initial_length / 2), verts[i - 1].pos.z);
+                Vector3 prevBelow = new Vector3(verts[i - 1].pos.x, verts[i - 1].pos.y - Mathf.Round(initial_length / 2), verts[i - 1].pos.z);
+                if (vertices[j].pos == above && verts[i - 1].pos.y > verts[i].pos.y)
+                {
+                    // There's an existing vertex above the current potential vertex
+                    // and you're trying to move down
+                    return false;
+                }
+                if (vertices[j].pos == below && verts[i - 1].pos.y < verts[i].pos.y)
+                {
+                    // There's an existing vertex below the current potential vertex
+                    // and you're trying to move up
+                    return false;
+                }
+                if (vertices[j].pos == prevAbove && verts[i - 1].pos.y < verts[i].pos.y)
+                {
+                    // There's an existing vertex above the previous potential vertex
+                    // and you're trying to move up
+                    return false;
+                }
+                if (vertices[j].pos == prevBelow && verts[i - 1].pos.y > verts[i].pos.y)
+                {
+                    // There's an existing vertex below the previous potential vertex
+                    // and you're trying to move down
+                    return false;
+                }
+
+                // Prevent from moving across if there is an existing vertex above/below that just moved or is about to move up/down
+                // Note: Not technically an overlap, but it makes the gemoetry look strange
+                if (j > 0 && vertices[j].pos == above && vertices[j - 1].pos.y < vertices[j].pos.y)
+                {
+                    // There's an existing vertex above the current potential vertex
+                    // and the existing vertex just moved up
+                    return false;
+                }
+                if (j < vertices.Count - 1 && vertices[j].pos == above && vertices[j + 1].pos.y < vertices[j].pos.y)
+                {
+                    // There's an existing vertex above the current potential vertex
+                    // and the existing vertex is about to move down
+                    return false;
+                }
+                if (j > 0 && vertices[j].pos == below && vertices[j - 1].pos.y > vertices[j].pos.y)
+                {
+                    // There's an existing vertex below the current potential vertex
+                    // and the existing vertex just moved down
+                    return false;
+                }
+                if (j < vertices.Count - 1 && vertices[j].pos == below && vertices[j + 1].pos.y > vertices[j].pos.y)
+                {
+                    // There's an existing vertex below the current potential vertex
+                    // and the existing vertex is about to move up
                     return false;
                 }
             }
@@ -501,7 +636,7 @@ public class LSystemController : MonoBehaviour
         float Nz = (pointToRotate.z - pivotPoint.z);
         angle = -angle * Mathf.PI / 180f;
         result = new Vector2(Mathf.Cos(angle) * Nx - Mathf.Sin(angle) * Nz + pivotPoint.x, Mathf.Sin(angle) * Nx + Mathf.Cos(angle) * Nz + pivotPoint.z);
-        
+
         result.x = Mathf.Round(result.x);
         result.y = Mathf.Round(result.y);
         return result;
--- FinalProject/Assets/RoomManager.cs
+++ FinalProject/Assets/RoomManager.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
+    // A list of all possible rooms, assigned in the editor
     public List<GameObject> rooms;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    /// <summary>
+    /// Assigns a room to each vertex in the dungeon based on it's connections to other rooms
+    /// </summary>
+    /// <param name="connections">A dictionary of vertices where each key is a unique vertex in the dungeon
+    ///     and each value a list of vertices that connect to the key</param>
+    /// <param name="end">The last vertex in the dungeon</param>
     public void BuildDungeon(Dictionary<Vector3, List<Vector3>> connections, Vector3 end)
     {
+        Vector3 prev = Vector3.zero;
         foreach (KeyValuePair<Vector3, List<Vector3>> k in connections)
         {
-            // Determine which connections this room has
+            // Determine which connections this room has and assign the previous vertex
             byte[] roomConnections = new byte[4] { 0, 0, 0, 0 };
+            bool first = true;
             foreach (Vector3 v in k.Value)
             {
+                if (first)
+                {
+                    // The first connection is always the previous vertex
+                    first = false;
+                    prev = v;
+                }
+
                 if (v.z > k.Key.z)
                 {
-                    // Connects up
+                    // Connects forward
                     roomConnections[0] = 1;
                 }
                 if (v.x > k.Key.x)
@@ -37,7 +42,7 @@ public class RoomManager : MonoBehaviour
                 }
                 if (v.z < k.Key.z)
                 {
-                    // Connects down
+                    // Connects back
                     roomConnections[2] = 1;
                 }
                 if (v.x < k.Key.x)
@@ -64,6 +69,56 @@ public class RoomManager : MonoBehaviour
             {
                 // This room is the end
                 roomString += " End";
+            }
+            else if (prev.y < k.Key.y && prev != k.Key)
+            {
+                // Moving up
+                roomString += " Up";
+                if (prev.z < k.Key.z)
+                {
+                    // Moving forward
+                    roomString += "Forward";
+                }
+                else if (prev.z > k.Key.z)
+                {
+                    // Moving back
+                    roomString += "Back";
+                }
+                else if (prev.x < k.Key.x)
+                {
+                    // Moving to right
+                    roomString += "Right";
+                }
+                else if (prev.x > k.Key.x)
+                {
+                    // Moving to left
+                    roomString += "Left";
+                }
+            }
+            else if (prev.y > k.Key.y && prev != k.Key)
+            {
+                // Moving down
+                roomString += " Down";
+                if (prev.z < k.Key.z)
+                {
+                    // Moving forward
+                    roomString += "Forward";
+                }
+                else if (prev.z > k.Key.z)
+                {
+                    // Moving back
+                    roomString += "Back";
+                }
+                else if (prev.x < k.Key.x)
+                {
+                    // Moving to right
+                    roomString += "Right";
+                }
+                else if (prev.x > k.Key.x)
+                {
+                    // Moving to left
+                    roomString += "Left";
+                }
             }
             else
             {
